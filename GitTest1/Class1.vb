c59b23a1f454c441741825c8c6239d860abe3867
[Alain-H] <al.high215@gmail.com>
2023-01-10 16:15:11 +0100

    Befehle in die Klasse hinzugefügt

--- GitTest1/Class1.vb
+++ GitTest1/Class1.vb
@@ -1,6 +1,16 @@
 ﻿Public Class Class1
-    'Das ist ein test der 2.Pusch einer Datei
-    ' Das ist der Test zu den Branches ob das wirklich funktioniert!
-    'Das ist aber nicht so richtig wie ich das sehe!!
-    'Das ist ein 2.Test
+    'git init = initialize project to use git
+    'git add . = add all changes to be saved
+    'git add *filename* = add single file to be saved
+    'git commit -m "message" = save changes with message
+    'git push origin master = push changes to github master
+    'git push origin new-branch = push changes to github new-branch
+    'git pull origin master = pull changes from gitHub
+    'git checkout -b new-branch = create a new branch
+    'git status = CheckBox status Of changes
+    'git log = Seek() all previous saved changes
+    'git checkout *commit hash*= travel back To old commit
+
+
+
 End Class
