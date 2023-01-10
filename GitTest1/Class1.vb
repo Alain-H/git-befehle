0a1538e251b56a871dd07cbd4abb3e0c1055cf73
Alain-H <93729636+Alain-H@users.noreply.github.com>
2023-01-10 15:54:37 +0100

    Update Class1.vb
    Test 2

--- GitTest1/Class1.vb
+++ GitTest1/Class1.vb
@@ -2,4 +2,5 @@
     'Das ist ein test der 2.Pusch einer Datei
     ' Das ist der Test zu den Branches ob das wirklich funktioniert!
     'Das ist aber nicht so richtig wie ich das sehe!!
+    'Das ist ein 2.Test
 End Class
